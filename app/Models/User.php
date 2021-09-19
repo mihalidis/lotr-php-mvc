36bca4e6ac1fdd0d181a1bd2beeb703c79661106
Pınar <pinarsuvac@gmail.com>
2021-09-19 22:35:50 +0300

    rmeoved unnecessary codes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,54 +6,6 @@ class User extends Db {
     protected $email;
     protected $password;
 
-    /**
-     * @return mixed
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param mixed $username
-     */
-    public function setUsername($username): void
-    {
-        $this->username = $username;
-    }
-
-    /**
-     * @param mixed $email
-     */
-    public function setEmail($email): void
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @param mixed $password
-     */
-    public function setPassword($password): void
-    {
-        $this->password = $password;
-    }
-
     // CRUD OPERATIONS
         public function create(array $data)
         {
